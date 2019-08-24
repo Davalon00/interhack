4f8fe78271f8cb6955c671eba7d034f31892343b
Camila Bezerril <camilabezerril@outlook.com>
2019-08-24 15:37:20 -0300

    cadastroevento

--- php/cadastrarEvento.php
+++ php/cadastrarEvento.php
@@ -1,17 +1,19 @@
 <?php
 
-    $nome_evento = $_POST['nome_evento'];
-    $datai = $_POST['datai'];
-    $horai = $_POST['horai'];
-    $dataf = $_POST['dataf'];
-    $horaf = $_POST['horaf'];
+    $nome_evento = $_POST[''];
+    $datai = $_POST[''];
+    $horai = $_POST[''];
+    $dataf = $_POST[''];
+    $horaf = $_POST[''];
+    $pos_usuario = $_POST[''];
+    $id_usuario = $_POST[''];
 	
-	$connect = mysqli_connect('127.0.0.1', 'root', '', 'cadastro');
-	$query = "INSERT INTO eventos (nome_usuario, datai, horai, dataf,horaf) VALUES ('$email', '$password', '$name',);";
+	$connect = mysqli_connect('127.0.0.1', 'root', '', 'Interhack');
+	$query = "INSERT INTO eventos (nome_evento, datai_evento, dataf_evento, horai_evento, horaf_evento, id_usuario, pos_usuario) VALUES ('$nome_evento', '$datai', '$horai', '$dataf', '$horaf','$pos_usuario');";
 	
 	$register = mysqli_query($connect, $query) or die('Erro ao cadastrar!');
 	
-	header("Location: cadastroUsuario.php");
+	header("Location: cadastroEvento.php");
 	exit();
 
 ?>
