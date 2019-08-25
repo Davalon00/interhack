f5d968a8ffbda9b5827b52489adf0c1fd189657f
Davalon00 <31051217+Davalon00@users.noreply.github.com>
2019-08-25 07:34:16 -0300

    asaaaaaaaa

--- exemplo pra form/Projeto_Leonardo/db_selecionar_funcionario.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$connect = mysqli_connect('127.0.0.1', 'root', '', 'ecommerce');
-	$query = "SELECT * FROM funcionarios";
-
-	$search = mysqli_query($connect, $query);
-
-?>
--- exemplo pra php/Sistema-de-cadastro-master/selecionarTrabalhador.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$connect = mysqli_connect('127.0.0.1', 'root', '', 'cadastro');
-	$query = "SELECT * FROM worker";
-
-	$search = mysqli_query($connect, $query);
-
-?>
